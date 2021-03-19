ac1a687f8fc7e2fdb1b5b78592674889f05d5296
shimosawa <shimon101018@gmail.com>
2021-03-19 19:27:13 +0900

    create users and level table

--- backend/database/migrations/2021_03_19_102203_add_foreign_key_level_id.php
+++ backend/database/migrations/2021_03_19_102203_add_foreign_key_level_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class AddForeignKeyLevelId extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->bigInteger('level_id')->unsigned()->nullable();
-            $table->text('code')->nullable();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign("level_id")
+                ->references("id")
+                ->on('level');
         });
     }
 
@@ -29,6 +27,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
